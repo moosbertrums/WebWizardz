d3b7dd9d35f4e0177efd94ed759c6da888b529d2
Moos Bertrums <moosbertrums@hotmail.com>
2025-05-27 22:13:19 +0200

    Angular + TailwindCSS setup completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,16 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <div class="flex flex-col h-screen bg-red-100 px-4">
+      <div class="flex flex-col gap-1 items-center justify-center py-10">
+        <h1 class="text-2x1 text-red-500 tracking-wide font-medium">
+          WebWizardz Poster Designer
+        </h1>
+        <p class="text-gray-500">
+          Ontwerp je eigen poster met behulp van deze tool!
+        </p>
+      </div>
+    </div>
     <router-outlet />
   `,
   styles: [],
